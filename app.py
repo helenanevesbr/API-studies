4abb5940ed080e0f6df4ca878a42872cff9465fc
Helena Neves <helenaneves.br@gmail.com>
2023-07-03 19:48:31 -0300

    First API endpoint

--- app.py
+++ app.py
@@ -1,29 +1,15 @@
-from flask import Flask, render_template
+from flask import render_template # Remove: import Flask
+from werkzeug import JSONEncoder
+import connexion
 
-'''
-This code gets a basic web server up and running and makes
-it respond with a home.html template, which will be served
-to a browser when navigating to the URL "/".
-'''
-
-app = Flask(__name__)
+app = connexion.App(__name__, specification_dir="./")
+#This tells Connexion which directory to look in for its configuration file
+app.add_api("swagger.yml")
+#To connect the OpenAPI configuration file (swagger.yml) with your Flask app
 
 @app.route("/")
 def home():
-    '''
-    You connect the URL route "/" to the home()
-    function by decorating it with @app.route("/"). A
-    decorator is a function that takes another function
-    and extends the behavior of the latter function
-    without explicitly modifying it.
-    '''
-
     return render_template("home.html")
-    '''
-    home() calls the Flask render_template() function to
-    get the home.html file from the templates directory
-    and return it to the browser.
-    '''
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ people.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+#Your server will run read_all() when it receives an HTTP request
+# to GET /api/people. The return value of read_all() is a list
+# of dictionaries with information about a person.
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+PEOPLE = {
+    "Fairy": {
+        "fname": "Tooth",
+        "lname": "Fairy",
+        "timestamp": get_timestamp(),
+    },
+    "Ruprecht": {
+        "fname": "Knecht",
+        "lname": "Ruprecht",
+        "timestamp": get_timestamp(),
+    },
+    "Bunny": {
+        "fname": "Easter",
+        "lname": "Bunny",
+        "timestamp": get_timestamp(),
+    }
+}
+
+def read_all():
+    return list(PEOPLE.values())
